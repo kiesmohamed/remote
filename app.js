a3aed6d7c4616490d6dc2b51a1deca41926e7beb
kies <kiesmohamed@outlook.com>
2016-12-04 12:31:39 +0100

    Nouveau port réponse de la vie, de l'univers et du reste Ajout pour test

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/contacts', contacts);
+//Ajout pour test
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
