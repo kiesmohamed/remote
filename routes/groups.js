936e72aec2a926ceab19fcc887c9f0b93d79ec49
kies <kiesmohamed@outlook.com>
2017-02-21 00:29:20 +0100

    Nouvelle route/groups Modification contacts.js

--- routes/groups.js
+++ routes/groups.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET contact listing. */
+/* GET groups listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
