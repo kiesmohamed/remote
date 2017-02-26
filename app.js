7a92c6def939895274c44bfe5ad35553b5210ef2
kies <kiesmohamed@outlook.com>
2017-02-26 14:11:45 +0100

    mohamed kies

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/groups', groups);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('That is Not Found');
+  var err = new Error('That is Not Found mokies');
   err.status = 404;
   next(err);
 });
