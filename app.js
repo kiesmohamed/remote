71f5c64ff7987551cc9bca2fb3dce83d4db4877e
kies <kiesmohamed@outlook.com>
2017-02-26 14:29:39 +0100

    changement du message d'erreur en cas de 404

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/contacts', contacts);
-app.use('/groups', groups);
+app.use('/refe ', groups);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
