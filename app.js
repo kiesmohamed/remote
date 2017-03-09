a39c4ec02b4d36ba90caca1a260754d5ccf95100
kies <kiesmohamed@outlook.com>
2017-03-09 02:13:24 +0100

    changment du message 2

--- app.js
+++ app.js
@@ -25,4 +25,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/contacts', contacts);
-app.use('/ mohamed kies', groups);
+app.use('/ mohamed', groups);
