11d2d4175144f1f23d066ec7f8bf8111b526d42b
kies <kiesmohamed@outlook.com>
2017-03-12 12:09:47 +0100

    add 2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'coffee');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
