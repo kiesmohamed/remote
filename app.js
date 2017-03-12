4d9ee7ed12fade366db8f109b3da5439368e90f0
kies <kiesmohamed@outlook.com>
2017-03-12 13:44:23 +0100

    df

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'coffee' );
+app.set('view engine', 'coffee' ); 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev')); 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
